9760fba20a77d1fb49db68f41a939e42eb9f6d48
KD-38-1 <john.paraiso@kdci.co>
2019-05-03 15:59:56 +0800

    how-it-works page: phase 1

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+
+	$(function () {
+	  $(document).scroll(function () {
+	    var $nav = $(".navbar-fixed-top");
+	    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	  });
+	});
+  
+});
